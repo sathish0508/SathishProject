d2cd6d647c6e540b2fa9847a8b7d750bcd9ca806
sathish0508 <104669038+sathish0508@users.noreply.github.com>
2024-12-08 08:48:46 +0530

    Update Twilio.py

--- Twilio.py
+++ Twilio.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": "https://sathish0508.github.io"}})
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
 
 # Paths for training images (relative paths)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get the current script directory
